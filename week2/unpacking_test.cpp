55ccd8880f3da7b1724673cc5debc6ed2612791d
CHOUMnote <rlawlgh1028@naver.com>
2022-09-06 03:35:36 +0900

    구조화된 바인딩

---
+++ week2/unpacking_test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+struct Entry {
+    string name;
+    int value;
+};
+Entry read_entry() {
+    string s;
+    int i;
+    cout << "in_string in_int : ";
+    cin >> s >> i;
+    return { s, i };
+}
+int main() {
+    auto e = read_entry();
+    cout << e.name << e.value <<endl;
+    auto [n, v] = read_entry(); //구조화된 바인딩 : 데이터 멤버에 사용 단, 접근 지정자 public
+    cout << n << v << endl;
+}
